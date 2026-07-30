b0d6bb17a67ee0c6c6bfd88a88d35b34952f3245
dane.riyell <daneriyell.deomampo@gmail.com>
2026-07-30 16:10:59 +0800

    Add database connection setup in db.py

---
+++ src/db.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
